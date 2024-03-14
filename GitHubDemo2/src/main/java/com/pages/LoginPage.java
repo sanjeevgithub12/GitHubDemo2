f8e5bb627d92b79e023bb9fd7589c1eedc50556b
Uday <Uday@DESKTOP-23F4QL6>
2024-03-14 22:09:46 +0530

    New Changes to Master Branch

--- GitHubDemo2/src/main/java/com/pages/LoginPage.java
+++ GitHubDemo2/src/main/java/com/pages/LoginPage.java
@@ -5,5 +5,7 @@ public class LoginPage {
 	   public void LoginPage() {
 		   System.out.println("This is login method");
 		   System.out.println("This is new comment added");
+		   System.out.println("This is new Message");
+		   
 	   }
 }
