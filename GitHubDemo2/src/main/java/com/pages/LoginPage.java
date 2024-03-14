94662cca4c4016b0accfedbb094e440adf3bda73
Uday <Uday@DESKTOP-23F4QL6>
2024-03-14 22:28:00 +0530

    Eclipse Changes added to NewTest.txt

---
+++ GitHubDemo2/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class LoginPage {
+
+	   public void LoginPage() {
+		   System.out.println("This is login method");
+		   System.out.println("This is new comment added");
+		   System.out.println("This is new Message");
+		   
+	   }
+}
